f018f6cff8c40114abd3e33959ff456d6302b4a3
Neto Marin <netomarin@gmail.com>
2020-04-23 09:41:44 -0300

    Navigation component basic sample

---
+++ Navigation/app/src/main/java/dev/netomarin/navigation/AnotherActivity.kt
@@ -0,0 +1,12 @@
+package dev.netomarin.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnotherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_another)
+    }
+}
